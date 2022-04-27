fbae9bcfe6de10db43ca9b72187897d59712ff7c
AyhamZaid <51743814+AyhamZaid@users.noreply.github.com>
2022-04-27 12:12:50 +0300

    Day 3

---
+++ p1.py
@@ -0,0 +1,210 @@
+
+#this is coding day
+
+
+from string import ascii_lowercase
+
+
+print("Hello World and welcome to re/start")
+
+if 50>20:
+    print("first bigger then secend number")
+if 50<20:    
+        print("first bigger")
+
+
+x =10
+y = "Hello Ali"
+
+print (x)
+print (y)
+
+"""hello and welcom to asfafas
+aasdsafafasd
+sfsdsdfsdfdffdf
+dsfsdfsdfsdf"""
+
+
+#Casting
+x =str(3)
+y =int(3)
+z =float(3)
+
+print(x)
+print(y)
+print(z)
+
+x = 6 
+y = "reama"
+z = True
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Single and double Quotes
+
+x = "k"
+print(x)
+y = 'm'
+print(y)
+
+a = 345
+A = "test"
+print(a)
+print(A)
+
+
+#VAr Names
+
+myname = "ayham"
+my_name = "ali"
+_my_name ="Sara"
+myName="Husam"
+MYTEST="DIEAA"
+myvar2="test"
+
+print(myname)
+print(my_name)
+print(_my_name)
+print(myName)
+print(MYTEST)
+print(myvar2)
+
+
+"""2myname = "ayham"
+
+myname = "test"
+
+my name = "test"""
+
+
+#Camel Case 
+myFirstName = "Ayham"
+
+#pascal Case 
+MyFirstName = "mohammed"
+
+#snake CAse 
+
+my_First_name="omer"
+
+
+#Assign Multiple VAlues
+
+x,y,z = "ps5" , "xbox" , "pc"
+print(x)
+print(y)
+print(z)
+
+
+x=y=z = "Games"
+print(x)
+print(y)
+print(z)
+
+
+#Unpack Coolction 
+Cars = ["bmw" , "honda" ,"fox"]
+x ,y ,z =Cars
+
+print(x)
+print(y)
+print(z)
+
+
+x ="AWS"
+y = "is"
+z = "awesome"
+
+print(x,y,z)
+
+
+x ="AWS "
+y = "is "
+z = "awesome"
+
+print(x+y+z)
+
+
+x = 4
+y = 34
+
+print(x + y)
+x*=10 
+#x+=10 
+print(x) # x = x + 10 
+#print(x * y)
+#print(x / y)
+
+"""
+There are three numeric type in pyhon 
+int
+float
+complex
+"""
+
+x = 1
+y =2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 1
+y =234567898765434567876543
+z = -233
+
+print(x)
+print(y)
+print(z)
+
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1.20
+y = 3.0
+z =-43.34
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = -67.5e200
+print(x)
+
+
+#Type Converion
+
+x = float(1)
+y = int(3.5)
+z = complex(x)
+
+print(x)
+print(y)
+print(z)
+
+#random number
+
+import random
+print(random.randrange(1,10))
+
+#Casting
+x =float(1)
+y =float(2.8)
+z =float("3")
+w =float("4.3")
+
+print(x)
+print(y)
+print(z)
+print(w)
+
